577acc62324de3fa31bdf93c0d4868bee0a44e64
Christy Bergman <cbergman@gmail.com>
2019-11-06 16:04:18 -0800

    Update plot_roc_curves.py

--- plot_roc_curves.py
+++ plot_roc_curves.py
@@ -1,36 +1,22 @@
-"""Plot train, valid, test ROC curves on top of each other in same plot"""
-
+import collections
 from collections import namedtuple
 
 # Nametuple used for feeding values to plots.
-SetValidation = namedtuple(
+SetValidation = collections.namedtuple(
     "SetValidation", ["y_true", "y_pred", "title"]
 )
 
 """Module to help when building classification related plot"""
-import numpy as np
-from matplotlib import pyplot as plt
-from sklearn import metrics
-from typing import Tuple
 
 
-def plot_precision_recall_vs_threshold(*tuples: Tuple[models.SetValidation]):
+def plot_precision_recall_vs_threshold(*tuples: SetValidation, posLabel):
     """Plot both precision and recall curves for different data
     sets (Typically: train, validation and test)
-
-    Keyword Arguments:
-        tuples {Tuple[pyazlo.plots.models.SetValidation]} -- Different
-            SetValidation instances, defining ground truths and
-            predictions for different sets of data.
-
-    Raises:
-        exceptions.PyazloPlotBadArgumentException: [description]
-        ValueError: [description]
     """
     no_tuples = len(tuples)
 
     if no_tuples < 1:
-        raise exceptions.PyazloPlotBadArgumentException(
+        raise ValueError(
             "You shall not pass with no inputs"
         )
 
@@ -41,7 +27,7 @@ def plot_precision_recall_vs_threshold(*tuples: Tuple[models.SetValidation]):
             pred_i = tuple_i.y_pred
             title_i = tuple_i.title
             precisions_i, recalls_i, thresholds_i = \
-                metrics.precision_recall_curve(true_i, pred_i)
+                metrics.precision_recall_curve(true_i, pred_i, pos_label=posLabel)
             ax_i.plot(
                 thresholds_i, precisions_i[:-1], "b--", label="Precision"
             )
@@ -56,26 +42,17 @@ def plot_precision_recall_vs_threshold(*tuples: Tuple[models.SetValidation]):
 
     else:
         error_message = "You should not pass more than 3 sets yet."
-        logger.error(error_message)
+        #logger.error(error_message)
         raise ValueError(error_message)
 
 
-def plot_rocs(*tuples: Tuple[models.SetValidation]):
+def plot_rocs(*tuples: SetValidation, posLabel):
     """Plots ROC curves for multiple sets of data.
-
-    Arguments:
-        tuples {pyazlo.plots.models.SetValidation}:
-            Tuple containing a list of validation sets objects.
-
-    Raises:
-        exceptions.PyazloPlotBadArgumentException: When there are no
-            tuples provided
-        ValueError: When more than 3 sets are provided.
     """
     no_tuples = len(tuples)
 
     if no_tuples < 1:
-        raise exceptions.PyazloPlotBadArgumentException(
+        raise ValueError(
             "You shall not pass with no inputs"
         )
 
@@ -83,12 +60,14 @@ def plot_rocs(*tuples: Tuple[models.SetValidation]):
         plt.figure()
 
         for tuple_i in tuples:
-            fpri, tpri, thi = metrics.roc_curve(tuple_i.y_true, tuple_i.y_pred)
+            fpri, tpri, thi = metrics.roc_curve(tuple_i.y_true
+                                                , tuple_i.y_pred
+                                                , pos_label=posLabel)
             auci = metrics.auc(fpri, tpri)
             label_i = "{}: {:0.4f}".format(
                 tuple_i.title, auci
+                
             )
-
             plt.plot(fpri, tpri, linewidth=2, label=label_i)
 
         plt.title('ROC Curve')
@@ -107,15 +86,3 @@ def plot_rocs(*tuples: Tuple[models.SetValidation]):
         error_message = "You should not pass more than 3 sets yet."
         logger.error(error_message)
         raise ValueError(error_message)
-
-
-# EXAMPLE FUNCTION CALL
-nameYActual = 'fraudLong'
-y_train_pred = trainDataAndPredictions['glm_p1'] #train fraud probability
-y_test_pred = validDataAndPredictions['glm_p1']  #valid fraud probability
-y_oot_pred = ootDataAndPredictions['glm_p1']     #oot fraud probability
-train_set_validation = SetValidation(trainDataAndPredictions[nameYActual], y_train_pred, "Train")
-valid_set_validation = SetValidation(validDataAndPredictions[nameYActual], y_test_pred, "Valid")
-oot_set_validation = SetValidation(ootDataAndPredictions[nameYActual], y_oot_pred, "OOT")
-
-plot_rocs(train_set_validation, valid_set_validation, oot_set_validation)
